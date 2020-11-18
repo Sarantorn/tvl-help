70dcd0802dd95ed7416ed43ef5208c1bc0965ba0
patcharin063 <61050893@kmitl.ac.th>
2020-11-18 22:09:23 +0700

    test add data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO PLACE VALUES (1,'D', 'Hello','Hello','Hello');
 INSERT INTO PLACE(place_name,province,category,link) VALUES('วัดลิงขบ','กรุงเทพ','วัด','www.aaa.com')
+INSERT INTO PLACE(place_name,province,category,link) VALUES('วัดลิงขบdd','กรุงเddทพ','วัดdd','www.aaadd.com')
